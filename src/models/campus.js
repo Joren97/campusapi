b013e632d53124e1b1b02a82d9abebdc1066a24f
Joren Synaeve <synaevejoren@gmail.com>
2021-12-10 11:29:46 +0100

    Add includes and sorting

--- src/models/campus.js
+++ src/models/campus.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const CampusSchema = new mongoose.Schema({
-    name: {type: String},
-    location: {type: String},
-    address: {type: String},
-    image: {type: String}
-  }, {
-    collection: 'campus'  
+  name: { type: String },
+  location: { type: String },
+  address: { type: String },
+  image: { type: String }
+}, {
+  collection: 'campus'
 });
 
 module.exports = mongoose.model('Campus', CampusSchema);
